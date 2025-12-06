97f2c0657475a91b911097e204641d3953751fdb
Brahim Boussada <brahimboussada@protonmail.com>
2025-12-07 00:10:18 +0300

    feat: create initial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="w-dvw h-dvh ">
+      <div className="flex items-center justify-center flex-col gap-1 h-full">
+        <h1>BrahimStudio</h1>
+        <p className="text-xs animate-pulse">Coming Soon</p>
+      </div>
+    </div>
+  );
 }
